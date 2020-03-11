8b3bf58ed695f40521c7c1f8ba61627edb171bfe
振彪 黄 <biao876990970@hotmail.com>
2020-03-11 17:54:01 +0800

    加入登录功能

--- BYSJ/BYSJ/BYSJ/MainPage.xaml.cs
+++ BYSJ/BYSJ/BYSJ/MainPage.xaml.cs
@@ -13,9 +13,34 @@ namespace BYSJ
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        string account;
+        string password;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            SQL_Class database = new SQL_Class();
+            if (database.IDCHeck(account, password))
+            {
+                DisplayAlert("Alert", "Login Success", "OK");
+            }
+            else
+            {
+                DisplayAlert("Alert", "Invalid password", "OK");
+            }
+        }
+
+        private void AccountInput(Entry sender, TextChangedEventArgs e)
+        {
+            this.account = sender.Text.ToString();
+        }
+
+        private void PasswordInput(Entry sender, TextChangedEventArgs e)
+        {
+            this.password = sender.Text.ToString();
         }
     }
 }
---
+++ BYSJ/BYSJ/BYSJ/SQL_Class.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BYSJ
+{
+    class SQL_Class
+    {
+        SqlConnectionStringBuilder sqlStr;
+        SqlConnection sqlConn;
+        public SQL_Class()
+        {
+            sqlStr = new SqlConnectionStringBuilder();
+            sqlStr.DataSource = "47.97.244.178";
+            sqlStr.InitialCatalog = "SVW";
+            sqlStr.UserID = "SA";
+            sqlStr.Password = "ZslNB2020";
+           
+            sqlConn = new SqlConnection(sqlStr.ToString());
+        }
+
+        public Boolean IDCHeck(string username, string password)
+        {
+            sqlConn.Open();
+            string sqlCommStr = "SELECT password FROM pwd WHERE name ='"+username+"'";
+            SqlCommand sqlComm = new SqlCommand(sqlCommStr, sqlConn);
+            SqlDataReader dataReader = sqlComm.ExecuteReader();
+            dataReader.Read();
+            
+
+            if ((String)dataReader["password"] == password)
+            {
+                dataReader.Close();
+                sqlConn.Close();
+                return true;
+            }
+            else
+            {
+                dataReader.Close();
+                sqlConn.Close();
+                return false;
+            }
+        }
+    }
+}
